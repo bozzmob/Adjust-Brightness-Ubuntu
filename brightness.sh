606d051f45666a6d3737258f2720d4b361f8eb19
satvik <satvik.jagannath7@gmail.com>
2015-07-26 23:10:03 +0530

    Added More Customizations

--- brightness.sh
+++ brightness.sh
@@ -1,1 +1,12 @@
-sudo su -c "echo 200 >/sys/class/backlight/intel_backlight/brightness"
+if [ "$1" == "l" ]
+then
+   sudo su -c "echo 500 >/sys/class/backlight/intel_backlight/brightness"
+elif [ "$1" == "m" ]
+then
+   sudo su -c "echo 1500 >/sys/class/backlight/intel_backlight/brightness"
+elif [ "$1" == "h" ]
+then
+   sudo su -c "echo 3000 >/sys/class/backlight/intel_backlight/brightness"
+else
+   sudo su -c "echo 1000 >/sys/class/backlight/intel_backlight/brightness"
+fi
